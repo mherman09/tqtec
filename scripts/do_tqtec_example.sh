2e12f28ec9205db3fb6e603529484a20dac6aaa7
mherman09 <matthew.w.herman@gmail.com>
2024-10-03 13:23:09 -0700

    Install TQTec scripts with executables
    - Update CMakeLists.txt so scripts are included with executables
      during build and installation
    - Update do_tqtec_example.sh script to be run from any location
      using installed versions of tqtec, readtqtec, and plotting
      scripts
    - Automatically determine times to plot geotherms
    - Update README

--- scripts/do_tqtec_example.sh
+++ scripts/do_tqtec_example.sh
@@ -1,31 +1,60 @@
 #!/bin/bash
 
 #####
-#   PARSE INPUT
+#   USAGE STATEMENT
 #####
-BASENAME=$1
-if [ "$BASENAME" == "" ]
+function usage() {
+    echo "Usage: $0 MODEL_FILE [...options...]" 1>&2
+    echo "MODEL_FILE         Input TQTec model file" 1>&2
+    echo "-plot:all          Generate a single figure file with all graphs" 1>&2
+    echo "-plot:individual   Generate an individual figure file for each graph" 1>&2
+    exit
+}
+
+
+#####
+#   PARSE INPUTS
+#####
+# Check for input model file argument
+MODEL_FILE=$1
+if [ "$MODEL_FILE" == "" ]
 then
-    echo "$0: no model basename specified" 1>&2
-    echo "Usage: $0 BASENAME" >&2
-    exit 1
+    echo "$0: no model input file specified" 1>&2
+    usage
 fi
 
-if [ ! -f $BASENAME.in ]
+# Check whether input model file exists
+if [ ! -f $MODEL_FILE ]
 then
-    echo "$0: no input file found named $BASENAME.in" 1>&2
-    echo "Usage: $0 BASENAME" >&2
-    exit 1
+    echo "$0: no input file found named $MODEL_FILE" 1>&2
+    usage
 fi
+
+# Parse arguments
+shift
+PLOT=ALL
+while [ "$1" != "" ]
+do
+    case "$1" in
+        -plot:all) PLOT="ALL";;
+        -plot:individual) PLOT="INDIVIDUAL";;
+        *) echo "No option $1" 1>&2; usage;;
+    esac
+    shift
+done
+
+
+# Extract model basename
+MODEL_BASENAME=`basename $MODEL_FILE .in`
 
 
 #####
 #   RUN TQTEC
 #####
 echo "$0: Running tqtec"
-../build/tqtec \
-    -f $BASENAME.in \
-    -o $BASENAME.out \
+tqtec \
+    -f $MODEL_FILE \
+    -o $MODEL_BASENAME.out \
     -timing timing.tmp \
     -geotherm geotherm.tmp \
     -v 2 || exit 1
@@ -35,8 +64,8 @@ echo "$0: Running tqtec"
 #   RUN READTQTEC
 #####
 echo "$0: Running readtqtec"
-../build/readtqtec \
-    $BASENAME.out \
+readtqtec \
+    $MODEL_BASENAME.out \
     -dep dep.tmp \
     -temp temp.tmp \
     -time time.tmp \
@@ -48,35 +77,48 @@ echo "$0: Running readtqtec"
 #####
 #   PLOT MODEL RESULTS
 #####
-# Horizon depth versus time
-../scripts/plot_tqtec_dep_vs_time.sh dep.tmp \
-    -timing timing.tmp
-# Horizon temperature versus time
-../scripts/plot_tqtec_temp_vs_time.sh temp.tmp \
-    -timing timing.tmp
-# Surface heat flow versus time
-../scripts/plot_tqtec_hf_vs_time.sh hf.tmp \
-    -timing timing.tmp
-# Horizon temperature versus depth
-../scripts/plot_tqtec_temp_vs_dep.sh dep.tmp temp.tmp \
-    -geotherm geotherm.tmp \
-    -geotherm:time 10 \
-    -geotherm:time 20
-# Horizon temperature contours superimposed on depth versus time
-../scripts/plot_tqtec_temp_contours.sh dep.tmp geotherm.tmp \
-    -timing timing.tmp
-# Horizon depth versus time
-../scripts/plot_tqtec_all.sh temp.tmp dep.tmp hf.tmp \
-    -timing timing.tmp \
-    -closure closure.tmp \
-    -geotherm geotherm.tmp \
-    -geotherm:time 10 \
-    -geotherm:time 20 || exit
+# Determine times to plot geotherms
+MODEL_TIME=`head -1 temp.tmp | awk '{print -$1}'`
+GEOTHERM_TIME_FLAG=
+for T in `seq 10 10 $MODEL_TIME`
+do
+    GEOTHERM_TIME_FLAG="$GEOTHERM_TIME_FLAG -geotherm:time $T"
+done
+
+# Plot individual figure files or single figure file
+if [ "$PLOT" == "INDIVIUDAL" ]
+then
+    # Horizon depth versus time
+    plot_tqtec_dep_vs_time.sh dep.tmp \
+        -timing timing.tmp
+    # Horizon temperature versus time
+    plot_tqtec_temp_vs_time.sh temp.tmp \
+        -timing timing.tmp
+    # Surface heat flow versus time
+    plot_tqtec_hf_vs_time.sh hf.tmp \
+        -timing timing.tmp
+    # Horizon temperature versus depth
+    plot_tqtec_temp_vs_dep.sh dep.tmp temp.tmp \
+        -geotherm geotherm.tmp \
+        $GEOTHERM_TIME_FLAG
+    # Horizon temperature contours superimposed on depth versus time
+    plot_tqtec_temp_contours.sh dep.tmp geotherm.tmp \
+        -timing timing.tmp
+elif [ "$PLOT" == "ALL" ]
+then
+    # Horizon depth versus time
+    plot_tqtec_all.sh temp.tmp dep.tmp hf.tmp \
+        -timing timing.tmp \
+        -closure closure.tmp \
+        -geotherm geotherm.tmp \
+        $GEOTHERM_TIME_FLAG || exit
+fi
+
 
 #####
 #   CLEAN UP
 #####
-test -d ${BASENAME}_results || mkdir ${BASENAME}_results
-cp *.pdf ${BASENAME}_results
-mv $BASENAME.out ${BASENAME}_results
+test -d ${MODEL_BASENAME}_results || mkdir ${MODEL_BASENAME}_results
+cp *.pdf ${MODEL_BASENAME}_results
+mv $MODEL_BASENAME.out ${MODEL_BASENAME}_results
 rm *.tmp
