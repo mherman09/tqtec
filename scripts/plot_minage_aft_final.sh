e83e646152c587763113b813f4929c6b8deb3a81
Matt Herman <mherman2@csub.edu>
2024-10-31 12:18:58 -0700

    Make AFT length distribution plots multi-page PDFs

--- scripts/plot_minage_aft_final.sh
+++ scripts/plot_minage_aft_final.sh
@@ -75,7 +75,7 @@ awk 'BEGIN{p=0}{
         } else {
             p = 0
         }
-    } 
+    }
     if (p==1) {
         print $0
     }
@@ -89,9 +89,8 @@ NMAX=`awk '{for(i=2;i<=NF;i++){print $i}}' aft_corrected_hist.tmp | gmt gmtinfo 
 #####
 #   PLOT RESULTS
 #####
-gmt set PS_MEDIA 100ix100i
+gmt set PS_MEDIA 8ix5i
 
-PSFILE=minage_aft_final.ps
 
 PROJ=-JX6i/3i
 CTMAX=`echo $NMAX | awk '{print $1*1.12}'`
@@ -104,39 +103,53 @@ gmt makecpt -Cmagma -T0/$NCOL2/1 > color_list.cpt
 awk '{if(NF==5){print $2}}' color_list.cpt > color_list.tmp
 
 
-# Initialize length distribution plot
-gmt psxy -T -K -Y95i > $PSFILE
-
+PSFILE_BASE=minage_aft_final
+EPOCH_TIME=`date "+%s"`
+I=001
 
 # Different panel for each distribution
 for COL in `seq 1 $NCOL`
 do
+
+    PSFILE=${PSFILE_BASE}_${EPOCH_TIME}_${I}.ps
+
+
+    # Initialize length distribution plot
+    gmt psxy -T -K -X1i -Y1i -P > $PSFILE
+
 
     echo $0: working on horizon $COL
 
     # Set color for this horizon
     COLOR=$(sed -ne "${COL}p" color_list.tmp)
 
-    # Plot gridlines
+    # Plot gridlines & part of frame
     gmt psbasemap $PROJ $LIMS -Byg -BSW -K -O \
         --MAP_GRID_PEN_PRIMARY=0.5p,205,4_2:0 >> $PSFILE
+    gmt psbasemap $PROJ $LIMS -Bxf1 -Byf+l"Count" -BWen -K -O >> $PSFILE
 
     # Mean and median lengths
     MEAN=`awk 'BEGIN{i='$COL'+1;sum=0;n=0}{
         n = n + $i
         len = $1
         sum = sum + $i*len
-    }END{print sum/n}' aft_corrected_hist.tmp`
-    echo $MEAN $CTMAX | awk '{print $1,0;print $1,$2}' |\
-        gmt psxy $PROJ $LIMS -W2p,$COLOR -K -O >> $PSFILE
+    }END{printf("%.1f"),sum/n}' aft_corrected_hist.tmp`
     MEDIAN=`awk 'BEGIN{i='$COL'+1;n=0}{
         for (j=1;j<=$i;j++) {
             n++
             len[n] = $1
         }
-    }END{print len[int(n/2)]}' aft_corrected_hist.tmp`
-    echo $MEDIAN $CTMAX | awk '{print $1,0;print $1,$2}' |\
-        gmt psxy $PROJ $LIMS -W2p,$COLOR,2_6:0 -K -O --PS_LINE_CAP=round >> $PSFILE
+    }END{printf("%.1f"),len[int(n/2)]}' aft_corrected_hist.tmp`
+    MEAN_JUST=`echo $MEAN $MEDIAN | awk '{if($1>$2){print "LB"}else{print "RB"}}'`
+    MEDIAN_JUST=`echo $MEAN $MEDIAN | awk '{if($1>$2){print "RB"}else{print "LB"}}'`
+    echo $MEAN $CTMAX | awk '{print $1,0;print $1,$2*1.2}' |\
+        gmt psxy $PROJ $LIMS -W2p,$COLOR -N -K -O --PS_LINE_CAP=round >> $PSFILE
+    echo $MEAN $CTMAX | awk '{print $1,$2*1.2,"Mean=" $1 "@~m@~m"}' |\
+        gmt pstext $PROJ $LIMS -F+f10,0,$COLOR,+j${MEAN_JUST} -D0/0.03i -N -K -O >> $PSFILE
+    echo $MEDIAN $CTMAX | awk '{print $1,$2*1.1; print $1,0}' |\
+        gmt psxy $PROJ $LIMS -W2p,$COLOR,2_6:0 -N -K -O --PS_LINE_CAP=round >> $PSFILE
+    echo $MEDIAN $CTMAX | awk '{print $1,$2*1.1,"Median=" $1 "@~m@~m"}' |\
+        gmt pstext $PROJ $LIMS -F+f10,0,$COLOR,+j${MEDIAN_JUST} -D0/0.03i -N -K -O >> $PSFILE
 
     # Plot length distributions
     awk 'BEGIN{i='$COL'+1}{
@@ -163,8 +176,8 @@ do
         gmt pstext $PROJ $LIMS -F+f12,3+cLB -D0.05i/5p -K -O >> $PSFILE
 
     # Plot & label frame
-    gmt psbasemap $PROJ $LIMS -Bxa1+l"Track Length (microns)" -Byf+l"Count" -BWeSn -K -O >> $PSFILE
-    echo Horizon $COL | gmt pstext $PROJ $LIMS -F+f20,3+cRT -D-0.05i/-0.04i -K -O >> $PSFILE
+    gmt psbasemap $PROJ $LIMS -Bxa1+l"Track Length (microns)" -BS -K -O >> $PSFILE
+    echo Horizon $COL | gmt pstext $PROJ $LIMS -F+f20,3,$COLOR=0.25p+cRT -D-0.05i/-0.04i -K -O >> $PSFILE
 
     #####
     #   PLOT TEMPERATURE HISTORY
@@ -235,20 +248,19 @@ do
             --MAP_LABEL_OFFSET=5p --MAP_ANNOT_OFFSET_PRIMARY=3p --MAP_TICK_LENGTH=3p >> $PSFILE
     fi
 
+    gmt psxy -T -O >> $PSFILE
+    I=`echo $I | awk '{printf("%03d"),$1+1}'`
 
-    gmt psxy -T -K -O -Y-4.0i >> $PSFILE
 done
 
 
 
-
-gmt psxy -T -O >> $PSFILE
-gmt psconvert $PSFILE -Tf -A
+gmt psconvert ${PSFILE_BASE}_${EPOCH_TIME}_*.ps -TF -F${PSFILE_BASE}
 
 
 #####
 # CLEAN UP
 #####
-rm $PSFILE
+rm ${PSFILE_BASE}_${EPOCH_TIME}_*.ps
 rm *.tmp
 rm color_list.cpt
